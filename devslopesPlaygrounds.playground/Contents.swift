9f998715bc635de550516aafaa0d7601c3ab2e0b
Raleigh Green <raleighgreen@gmail.com>
2017-05-18 15:45:08 -0400

    finish numbers module

--- devslopesPlaygrounds.playground/Contents.swift
+++ devslopesPlaygrounds.playground/Contents.swift
@@ -2,7 +2,47 @@
 
 import UIKit
 
-// Type inference
-var str1 = "Hello, playground"
+// Numbers
 
+// Intergers
+var age = 30 // Type inference - reccomended
 
+var weight: Int = 200 // Explicitly declared type
+
+// If need a long number, use a Double
+var someLongNumb: Double = 91872634987612398764198237647651237864518
+
+// Also use a Double for decimal points - this is preferred to Floats, 
+// So, always use Doubles 
+
+var milesRan = 56.45
+
+var pi: Float = 3.14
+
+// Arithmetic Operators
+// + - / *
+
+var area = 15 * 20
+
+var sum = 5 + 6
+
+var diff = 10 - 3
+
+var div = 12 / 3
+
+var remainder = 12 % 5
+
+var result = "The result of 12 / 5 is \(div) with a remainder of \(remainder)"
+
+// How to determine if a number is even or odd
+
+var randomNum = 14
+
+if randomNum % 2 == 0 {
+    print("this is an even number")
+} else {
+    print("this is an odd number")
+}
+
+// Order of opporations applies
+var result2 = 15 * ((5 + 7) / 3)
